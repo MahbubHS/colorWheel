774f3e4fbcc3c0e57d7c2037fa6f6fa8e9ae55ca
MD Mahbub Hasan Shad <137253486+MahbubHS@users.noreply.github.com>
2026-03-27 17:45:38 +0600

    Update: update the final project

--- components/ColorWheel/Header.tsx
+++ components/ColorWheel/Header.tsx
@@ -42,7 +42,7 @@ const Header = memo(function Header({ mode, onModeChange }: Props) {
         <SelectTrigger className="w-32 shrink-0 sm:w-40">
           <SelectValue placeholder="Mode" />
         </SelectTrigger>
-        <SelectContent className="bg-black/2" position="popper" sideOffset={1}>
+        <SelectContent className="bg-black/10 [&_[data-state=checked]]:bg-white/10" position="popper" sideOffset={1}>
           {MODE_KEYS.map((key) => (
             <SelectItem key={key} value={key}>
               {MODES[key].icon} {MODES[key].label}
